a6dee8d993ffa203014b84c5b0d8803d5018b404
GuillermoElectrico <guillermoelectrico@outlook.com>
2018-02-24 21:58:43 +0100

    Update conn.php

--- Web-Front-End/conn.php
+++ Web-Front-End/conn.php
@@ -3,6 +3,5 @@ $host   ='X.X.X.X';
 $ussBD	='db-user';
 $pasBD	='db-pass';
 $BBDD	  ='db-name';
-$conn = mysqli_connect($host, $ussBD, $pasBD, $BBDD)
-  
+$conn = mysqli_connect($host, $ussBD, $pasBD, $BBDD)  
 ?>
